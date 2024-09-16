9ebfd15939b2d2b7521737a533275edb55b72d4f
Simon Sprouse <simonhsprouse@gmail.com>
2024-09-16 16:30:08 -0500

    css changes

---
+++ react-app/src/Graph.js
@@ -0,0 +1,185 @@
+import { useState, useEffect, useRef } from 'react'
+
+function Graph() { 
+
+    const canvasRef = useRef(null);
+    const height = 400;
+
+    const [verticalLines, setVerticalLines] = useState([]);
+
+    const initialData = Array.from({length: 20}, () => [Math.random() * height, 0, 0])
+    const [dataPoints, setDataPoints] = useState(initialData);
+
+    function addVeriticalLine(event) { 
+        
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+
+
+        setVerticalLines(prevLines => {
+            const newLines = [...prevLines, x].sort((a, b) => a - b);
+            updatePointIds(newLines);
+            return newLines;
+        });
+
+    }
+
+
+    function updatePointIds(verticalLines) { 
+        const canvas = canvasRef.current;
+        const x_step = canvas.width / (dataPoints.length - 1);
+
+        // dataPoints.forEach(point => console.log(point));
+
+        const updatedPoints = dataPoints.map((point, index) => { 
+            const x = x_step * index; // x pos on graph
+            let newId = 0;
+
+            for (let i = 0; i < verticalLines.length; i++) {
+                if (x > verticalLines[i]) { 
+                    newId = i + 1;
+                }
+                else { 
+                    break;
+                }
+            }
+
+            return [point[0], newId, point[2]];
+        });
+
+        // updatedPoints.forEach(point => console.log(point));
+
+        setDataPoints(updatedPoints);
+
+    }
+
+
+    function drawGraph() {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        const x_step = canvas.width / (dataPoints.length - 1);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.lineWidth = 2;
+
+        // draw datapoints graph
+        dataPoints.forEach((point, index) => { 
+
+
+            const x = index * x_step;
+            const price = point[0];
+            const id = point[1];
+
+            if (index > 0) { 
+
+                
+                const prevX = (index - 1) * x_step;
+
+                const prevPoint = dataPoints[index - 1];
+                const prevPrice = prevPoint[0];
+                const prevId = prevPoint[1]
+
+
+                if (id % 4 == 0) {
+                    ctx.strokeStyle = "blue";
+                }
+                else if (id % 4 == 1) {
+                    ctx.strokeStyle = "red";
+                }
+                else if (id % 4 == 2) {
+                    ctx.strokeStyle = "green";
+                }
+                else if (id % 4 == 3) {
+                    ctx.strokeStyle = "yellow";
+                }
+                
+                ctx.beginPath();
+                ctx.moveTo(prevX, prevPrice);
+                ctx.lineTo(x, price);
+                ctx.stroke();
+            }
+            
+        });
+
+
+        // draw vertical lines
+        verticalLines.forEach(line => {
+            
+            ctx.beginPath();
+            ctx.moveTo(line, 0);
+            ctx.lineTo(line, canvas.height);
+            ctx.strokeStyle = "black";
+            ctx.linewidth = 2;
+            ctx.stroke();
+        })
+    }
+
+
+
+    // draw graph
+    useEffect(() => { 
+        console.log("detected a change in dataPoints");
+        drawGraph();
+    }, [verticalLines, dataPoints]);
+
+
+    // add event listener to graph
+    useEffect(() => { 
+        const canvas = canvasRef.current;
+        canvas.addEventListener("click", addVeriticalLine);
+
+        return () => { 
+            canvas.removeEventListener("click", addVeriticalLine);
+        }
+    }, []);
+
+
+    function handleClick() {
+        dataPoints.forEach(point => console.log(point));
+    }
+
+
+
+    return (
+        <>
+            <canvas id="graph" ref={canvasRef} width="800" height="400"></canvas>
+            <button onClick={handleClick}>Log</button>
+        </>
+    )
+}
+
+export default Graph;
+
+
+
+
+
+
+
+
+/*
+
+
+Ok Here's what the fuck is happening. 
+
+So right now the lines get drawn but aren't stored anywhere. 
+
+I need the data array to contain three items per point: 
+    - price
+    - id
+    - class
+
+so the array should look like: 
+
+    [[58, 0, 1], [50, 0, 1], [60, 1, 3]]
+
+Price should be generated randomly for now. 
+Id will be unique for each section bounded by vertical lines
+Class will be generated randomly for now 0-k
+
+
+*/
